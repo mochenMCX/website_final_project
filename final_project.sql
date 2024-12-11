901b642bb3bea45a21ca09b0ddbb27772596d69d
cloud1204 <63010089+cloud1204@users.noreply.github.com>
2024-12-11 22:35:26 +0800

    Update readme & .sql (#2)
    * add game info table, lobby system, changed sql structures
    
    * updated sql
    
    * updated readme

--- final_project.sql
+++ final_project.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2024-12-10 13:23:38
+-- 產生時間： 2024 年 12 月 11 日 15:22
 -- 伺服器版本： 10.4.32-MariaDB
--- PHP 版本： 8.2.12
+-- PHP 版本： 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,15 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `games`
+--
+
+CREATE TABLE `games` (
+  `game_id` int(11) NOT NULL,
+  `start_time` date NOT NULL DEFAULT current_timestamp(),
+  `host_player` varchar(256) DEFAULT NULL,
+  `host_player_color` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `movement`
 --
 
 CREATE TABLE `movement` (
-  `color` varchar(255) NOT NULL,
-  `chess` varchar(255) NOT NULL,
-  `start_pos` varchar(255) NOT NULL,
-  `end_pos` varchar(255) NOT NULL,
-  `round` int(255) NOT NULL
+  `color` varchar(255) DEFAULT NULL,
+  `chess` varchar(255) DEFAULT NULL,
+  `start_pos` varchar(255) DEFAULT NULL,
+  `end_pos` varchar(255) DEFAULT NULL,
+  `round` int(255) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `time_stamp` datetime NOT NULL DEFAULT current_timestamp(),
+  `white_time` int(11) DEFAULT NULL,
+  `black_time` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -40,20 +57,26 @@ CREATE TABLE `movement` (
 --
 
 --
+-- 資料表索引 `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`game_id`);
+
+--
 -- 資料表索引 `movement`
 --
 ALTER TABLE `movement`
-  ADD PRIMARY KEY (`round`);
+  ADD PRIMARY KEY (`round`,`game_id`);
 
 --
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
 
 --
--- 使用資料表自動遞增(AUTO_INCREMENT) `movement`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `games`
 --
-ALTER TABLE `movement`
-  MODIFY `round` int(255) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `games`
+  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
